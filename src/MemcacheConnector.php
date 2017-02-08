901570cc582c86da6655e19fdbaef9daeab3f1d7
gengkang <gengkang@xnw.com>
2017-02-08 10:13:33 +0800

    Rejected commit: Default

--- src/MemcacheConnector.php
+++ src/MemcacheConnector.php
@@ -3,46 +3,45 @@
 use Memcache;
 use RuntimeException;
 
-class MemcacheConnector {
+class MemcacheConnector
+{
 
-	/**
-	 * Create a new Memcache connection.
-	 *
-	 * @param  array  $servers
-	 * @return \Memcache
-	 *
-	 * @throws \RuntimeException
-	 */
-	public function connect(array $servers)
-	{
-		$memcache = $this->getMemcache();
+    /**
+     * Create a new Memcache connection.
+     *
+     * @param  array $servers
+     * @return \Memcache
+     *
+     * @throws \RuntimeException
+     */
+    public function connect(array $servers)
+    {
+        $memcache = $this->getMemcache();
 
-		// For each server in the array, we'll just extract the configuration and add
-		// the server to the Memcached connection. Once we have added all of these
-		// servers we'll verify the connection is successful and return it back.
-		foreach ($servers as $server)
-		{
-			$memcache->addServer(
-				$server['host'], $server['port'], $server['weight']
-			);
-		}
+        // For each server in the array, we'll just extract the configuration and add
+        // the server to the Memcached connection. Once we have added all of these
+        // servers we'll verify the connection is successful and return it back.
+        foreach ($servers as $server) {
+            $memcache->addServer(
+                $server['host'], $server['port'], $server['weight']
+            );
+        }
 
-		if ($memcache->getVersion() === false)
-		{
-			throw new RuntimeException("Could not establish Memcache connection.");
-		}
+        if ($memcache->getVersion() === false) {
+            throw new RuntimeException("Could not establish Memcache connection.");
+        }
 
-		return $memcache;
-	}
+        return $memcache;
+    }
 
-	/**
-	 * Get a new Memcached instance.
-	 *
-	 * @return \Memcached
-	 */
-	protected function getMemcache()
-	{
-		return new Memcache;
-	}
+    /**
+     * Get a new Memcached instance.
+     *
+     * @return \Memcached
+     */
+    protected function getMemcache()
+    {
+        return new Memcache;
+    }
 
 }
